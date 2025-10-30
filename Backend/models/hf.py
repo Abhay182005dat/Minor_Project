99878757768416ed3f3e0def0c16e1d9ee0dafca
Abhay S N <abhay182005@gmail.com>
2025-10-30 23:21:54 +0530

    minor changes

--- Backend/models/hf.py
+++ Backend/models/hf.py
@@ -1,9 +1,14 @@
 from huggingface_hub import HfApi
+import os
 
 api = HfApi()
+
+repo_id = os.getenv("REPO_ID")
+
+
 api.upload_file(
     path_or_fileobj="Model.h5",                     # local file path
     path_in_repo="Model.h5",                        # where it appears in repo
-    repo_id="Abhay1831/deepfake_v1",     # your HF repo name
+    repo_id=repo_id,     # your HF repo name
     repo_type="model"
 )
